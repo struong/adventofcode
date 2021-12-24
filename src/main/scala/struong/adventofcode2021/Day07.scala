c77d43c334e6b31186fbf0dc6ab86a4a9f0e236b
struong <45595406+struong@users.noreply.github.com>
2021-12-24 23:52:01 +0000

    Day 07 p2

--- src/main/scala/struong/adventofcode2021/Day07.scala
+++ src/main/scala/struong/adventofcode2021/Day07.scala
@@ -3,17 +3,16 @@ package struong.adventofcode2021
 final case class Crabman(fuelCost: Int, position: Int)
 
 object Crabs {
-  def fuelCost(input: Seq[Int]): Crabman = {
+  def fuelCost(input: Seq[Int], fastBurn: Boolean): Crabman = {
     val max = input.max
     val min = input.min
 
-    println(s"max = ${max}")
-    println(s"min = ${min}")
-
     Seq.range(min, max).foldLeft(Crabman(Int.MaxValue - 1, Int.MaxValue)) {
       case (lowestCrabman, position) =>
-        val newFuelCost = calculateFuelCost(input, position)
-        if(lowestCrabman.fuelCost > newFuelCost) {
+        val newFuelCost =
+          if (fastBurn) calculateFuelFastCost(input, position)
+          else calculateFuelCost(input, position)
+        if (lowestCrabman.fuelCost > newFuelCost) {
           Crabman(newFuelCost, position)
         } else {
           lowestCrabman
@@ -21,14 +20,24 @@ object Crabs {
     }
   }
 
-  private def calculateFuelCost(input: Seq[Int], target: Int): Int = {
+  def calculateFuelCost(input: Seq[Int], target: Int): Int = {
     input.foldLeft(0) {
       case (acc, current) =>
         acc + math.abs(target - current)
     }
   }
-}
 
+  def calculateFuelFastCost(input: Seq[Int], target: Int): Int = {
+    input.foldLeft(0) {
+      case (acc, current) =>
+        val distance = math.abs(current - target) + 1
+
+        val total = (1 until distance).sum
+        acc + total
+    }
+  }
+
+}
 
 object Day07 {
 
@@ -41,8 +50,9 @@ object Day07 {
       .map(_.toInt)
       .toSeq
 
-
-    val result = Crabs.fuelCost(input)
+    val result = Crabs.fuelCost(input, false)
     println(s"result = ${result}")
+    val result2 = Crabs.fuelCost(input, true)
+    println(s"result2 = ${result2}")
   }
 }
--- src/test/scala/struong/adventofcode2021/Day07Spec.scala
+++ src/test/scala/struong/adventofcode2021/Day07Spec.scala
@@ -5,8 +5,23 @@ import org.scalatest.matchers.should.Matchers
 
 class Day07Spec extends AnyFunSuite with Matchers {
 
-  test("fuel usage to position 2") {
+  test("constant fuel usage for target 2") {
     val input = Seq(16, 1, 2, 0, 4, 2, 7, 1, 2, 14)
-    Crabs.fuelCost(input) shouldBe Crabman(37, 2)
+    Crabs.calculateFuelCost(input, 2) shouldBe 37
+  }
+
+  test("optimal fuel usage using constant burn") {
+    val input = Seq(16, 1, 2, 0, 4, 2, 7, 1, 2, 14)
+    Crabs.fuelCost(input, false) shouldBe Crabman(37, 2)
+  }
+
+  test("fast fuel usage for target 2") {
+    val input = Seq(16, 1, 2, 0, 4, 2, 7, 1, 2, 14)
+    Crabs.calculateFuelFastCost(input, 5) shouldBe 168
+  }
+
+  test("optimal fuel usage using fast burn") {
+    val input = Seq(16, 1, 2, 0, 4, 2, 7, 1, 2, 14)
+    Crabs.fuelCost(input, true) shouldBe Crabman(168, 5)
   }
 }
